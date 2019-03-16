bedf79bae9972db1b0813e6a2397a424357cc7a8
Maria Nosareva <mashunya_19-97@mail.ru>
2019-03-16 11:52:14 +0300

    fourth example

---
+++ flask/fourth-example/first-app/web_app/models.py
@@ -0,0 +1,17 @@
+from web_app import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author')
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
